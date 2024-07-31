fa8415709e6607c2d7868dd7425d4a9eb4911031
acespare <asfsjj123@gmail.com>
2024-07-31 17:55:56 +0800

    git init

---
+++ vite-sample/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import { ref } from "vue";
+
+ const num = ref(3);
+  function ClickMe() {
+  num.value++
+}
+</script>
+
+<template>
+  <div>
+    
+    {{num}}
+<button type="button" v-on:click="ClickMe">點我</button>
+
+  </div>
+</template>
+
+
